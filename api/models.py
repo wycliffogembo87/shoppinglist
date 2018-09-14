32967c1d879019bc0edfc5d5dac8fa0d1cc36709
wycliffogembo87 <wycliffogembo87@gmail.com>
2018-09-14 15:01:10 +0300

    Added and succesfully tested Shoppinglist model

--- api/models.py
+++ api/models.py
@@ -4,4 +4,14 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Shoppinglist(models.Model):
-    pass
+    """This class represents the Shoppinglist model."""
+    
+    name = models.CharField(max_length=255, blank=False, unique=True)
+    budget = models.DecimalField(decimal_places=2, max_digits=20, default=0.00)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """Return a humaan readable representation of the model instance."""
+
+        return {}.format(self.name)
