a27f7980f633c9a28ce6300c35e01273bfe0c92f
wycliffogembo87 <wycliffogembo87@gmail.com>
2018-09-16 00:04:57 +0300

    Added the shoppingitem serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,13 +1,22 @@
 from rest_framework import serializers
 
-from .models import Shoppinglist
+from .models import Shoppinglist, shoppingitem
 
+
+class ShoppingitemSerializer(serializers.ModelSerializer):
+    """Serializer to map the shoppingitem model instance into JSON format."""
+
+    class Meta:
+        """Meta class to map serializer's fields with the model fields."""
+        model = Shoppingitem
+        fields = ('id', 'name')
 
 class ShoppinglistSerializer(serializers.ModelSerializer):
-    """Serializer to map the model instance into JSON format."""
+    """Serializer to map the shoppinglist model instance into JSON format."""
+    shoppingitems = ShoppingitemSerializer(many=True, read_only=True)
 
     class Meta:
         """Meta class to map serializer's fields with the model fields."""
         model = Shoppinglist
-        fields = ('id', 'name', 'budget', 'date_created', 'date_modified')
-        read_only_fields  = ('date_created', 'date_modified')
+        fields = ('id', 'name', 'budget', 'shoppingitems')
+
