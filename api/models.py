8a08a5d9b330ba050da7059140e6a2ae47e38d32
wycliffogembo87 <wycliffogembo87@gmail.com>
2018-09-15 23:23:18 +0300

    Added the shoppingitem model

--- api/models.py
+++ api/models.py
@@ -12,6 +12,19 @@ class Shoppinglist(models.Model):
     date_modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        """Return a humaan readable representation of the model instance."""
+        """Return a human readable representation of the model instance."""
+
+        return {}.format(self.name)
+
+class ShoppingItem(models.Model):
+    """This class represents the ShoppingItem model."""
+    
+    shoppinglist = models.ForeignKey(Shoppinglist, related_name="shoppingitems")
+    name = models.CharField(max_length=255, blank=False, unique=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """Return a human readable representation of the model instance."""
 
         return {}.format(self.name)
