71d1ee189c37788aed2db906b8c46fa696969859
wycliffogembo87 <wycliffogembo87@gmail.com>
2018-09-15 22:54:16 +0300

    Added the shppinglist create behaviour of our rest api

--- api/views.py
+++ api/views.py
@@ -3,4 +3,16 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics
+from .serializers import ShoppinglistSerializer
+from .models import Shoppinglist
+
+
+class CreateView(generics.ListCreateAPIView):
+    """This class defines the create behaviour of our rest api."""
+    queryset = Shoppinglist.objects.all()
+    serializer_class = ShoppinglistSerializer
+
+    def perform_create(self, serializer):
+        """Save the post data when creating a new shoppinglist."""
+        serializer.save()
